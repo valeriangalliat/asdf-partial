c1b8d6b417b17d3b7fd8197b79c47e46b9bac5bb
Val <val@codejam.info>
2022-05-16 11:43:39 -0400

    Better UX

--- asdf-use.sh
+++ asdf-use.sh
@@ -3,24 +3,24 @@ ASDF_USE_DIR=$(cd "$(dirname "$0")" && pwd)
 eval "${$(declare -f asdf)/asdf/__asdf_use_orig}"
 
 asdf() {
-    plugin="$1"
+    command="$1"
 
     if [ "$#" -gt 0 ]; then
         shift
     fi
 
-    case "$plugin" in
+    case "$command" in
         use)
             if [ "$2" = latest ]; then
                 __asdf_use_orig shell "$@"
             else
-                eval "$("$ASDF_USE_DIR/asdf-use-shell" "$@")"
+                eval "$("$ASDF_USE_DIR/asdf-use-shell" "$@" || echo false)"
             fi
 
             ;;
 
         *)
-            __asdf_use_orig "$plugin" "$@"
+            __asdf_use_orig "$command" "$@"
             ;;
     esac
 }
